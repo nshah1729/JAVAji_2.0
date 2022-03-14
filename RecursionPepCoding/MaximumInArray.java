a847ffd51724d5290a1ac52b38e0319bcd5d9e26
nishant <nishantshah1729@gmail.com>
2022-03-14 08:53:31 +0530

    done

---
+++ RecursionPepCoding/MaximumInArray.java
@@ -0,0 +1,21 @@
+package com.company.RecursionPepCoding;
+
+import java.util.Scanner;
+
+public class MaximumInArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] a={1,2,3,235,5,19, 10, 8, 17,235, 9};
+        System.out.print("THE MAXIMUM IS: "+Arrmax(a,0));
+    }
+
+    private static int Arrmax(int[] a, int i) {
+        //Base Case:
+        if(i==a.length)return a[i-1];
+
+        int max=a[i];
+        int temp=Arrmax(a,i+1);
+        if(Arrmax(a,i+1)>max)max=Arrmax(a,i+1);
+        return max;
+    }
+}
