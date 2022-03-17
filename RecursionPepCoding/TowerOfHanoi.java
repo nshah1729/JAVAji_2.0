c63e8e5a1666ae7d5458360678cfacb496b49f3d
nishant <nishantshah1729@gmail.com>
2022-03-17 15:47:35 +0530

    done

--- RecursionPepCoding/TowerOfHanoi.java
+++ RecursionPepCoding/TowerOfHanoi.java
@@ -7,5 +7,16 @@ public class TowerOfHanoi {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter n: ");
         int n=sc.nextInt();
+        int t1= sc.nextInt();
+        int t2= sc.nextInt();
+        int t3= sc.nextInt();
+        toh(n,t1,t2,t3);
+    }
+
+    private static void toh(int n, int t1, int t2, int t3) {
+        if(n==0)return;
+        toh(n-1,t1,t3,t2);
+        System.out.println(n+"["+t1+"-->"+t2+"]");
+        toh(n-1,t3,t2,t1);
     }
 }
