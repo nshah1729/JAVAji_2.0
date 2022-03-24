5a9f4eb2421677e1f4b11eb42c4471172d27a041
nishant <nishantshah1729@gmail.com>
2022-03-24 13:37:42 +0530

    done

---
+++ Sorting/CycleSort/FirstMissingPositive.java
@@ -0,0 +1,16 @@
+package com.company.Sorting.CycleSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int[] a={1,2,2,4};
+        System.out.println(Arrays.toString(miss(a)));
+    }
+    private static int miss(int[] a) {
+
+    }
+
+}
