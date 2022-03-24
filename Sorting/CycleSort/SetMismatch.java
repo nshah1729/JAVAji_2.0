c8c9538a16d5a6d20d2fd6bd6bf99f885f8b519f
nishant <nishantshah1729@gmail.com>
2022-03-24 13:37:24 +0530

    done

--- Sorting/CycleSort/SetMismatch.java
+++ Sorting/CycleSort/SetMismatch.java
@@ -1,37 +1,29 @@
 package com.company.Sorting.CycleSort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class SetMismatch {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int[] a={1,2,2,4};
-        System.out.println((mismatch(a)));
+        System.out.println(Arrays.toString(mismatch(a)));
     }
 
-    private static ArrayList mismatch(int[] a) {
-        HashSet<Integer> set=new HashSet<>();
-        ArrayList<Integer> list=new ArrayList<>();
-        for (int i = 0; i < a.length; i++) {
+    private static int[] mismatch(int[] a) {
+        int i=0;
+        while(i<a.length){
             int correctIndex=a[i]-1;
-            while(a[i]!=i+1){
-                if(set.contains(a[i]))continue;
-                else{
-                    swap(a,i,correctIndex);
-                    set.add(a[i]);
-                }
+            if(a[i]!=a[correctIndex])swap(a,i,correctIndex);
+            else i++;
+        }
+        int[] ans=new int[2];
+        for (int j = 0; j < a.length; j++) {
+            if(a[j]!=j+1){
+                ans[0]=a[j];
+                ans[1]=j+1;
             }
         }
-        for (int i = 0; i < a.length; i++) {
-                if(a[i]!=i+1){
-                    list.add(i+1);
-                    list.add(a[i]);
-                }
-        }
-        return list;
+        return ans;
     }
 
     private static void swap(int[] a, int i, int correctIndex) {
