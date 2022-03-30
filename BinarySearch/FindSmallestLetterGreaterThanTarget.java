6fa6f306fb27464f11ee1dc1869937cb0eff1f5a
nishant <nishantshah1729@gmail.com>
2022-03-30 13:44:50 +0530

    done

--- BinarySearch/FindSmallestLetterGreaterThanTarget.java
+++ BinarySearch/FindSmallestLetterGreaterThanTarget.java
@@ -7,16 +7,18 @@ return the smallest character in the array that is larger than target.
 Note that the letters wrap around.
 For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.*/
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class FindSmallestLetterGreaterThanTarget {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         char[] letters={'a','b','f','h'};
-        char c='d';
-        System.out.println(small(letters,c));
-    }
+        char c='b';
+        System.out.println(small_1(letters,c));
 
+    }
+    //Method 1:
     private static char small(char[] letters, char c) {
         int s=0,e=letters.length-1;
         while(s<=e){
@@ -25,5 +27,16 @@ public class FindSmallestLetterGreaterThanTarget {
             else s=m+1;
         }
         return letters[s % letters.length];
+    }
+    //Method 2:
+    private static char small_1(char[] letters, char c) {
+        int s=0,e=letters.length-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(letters[m]>c)e=m-1;
+            else s=m+1;
+            if(s==letters.length)return letters[0];
+        }
+        return letters[s];
     }
 }
---
+++ BinarySearch/SearchAnElementInInfiniteSortedArray.java
@@ -0,0 +1,17 @@
+package com.company.BinarySearch;
+
+import java.util.Scanner;
+
+public class SearchAnElementInInfiniteSortedArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] a={2,3,4,6,9};
+        int t=4;
+        System.out.println(search(a,4));
+
+    }
+
+    private static int search(int[] letters, int c) {
+        return 1;
+    }
+}
