58fa9e74ce7005b1abc60d762a9101b459e8edb9
nishant <nishantshah1729@gmail.com>
2022-04-08 21:58:28 +0530

    done

--- BinarySearch/FindPeakInMountainArray.java
+++ BinarySearch/FindPeakInMountainArray.java
@@ -7,19 +7,21 @@ public class FindPeakInMountainArray {
     }
 
     private static int peak(int[] a) {
-        int s=0,e=a.length-1,ans=-1;
+        int s=0,e=a.length-1;
         while(s<e){
             int mid=s+(e-s)/2;
             //You're in the Descending part of the array...
             // but the answer might lie on the lhs...
             // though this might be a potential ans
             if(a[mid]>a[mid+1]){
-                ans=mid;
                 e=mid;
             }
             else s=mid+1;//Bcoz, we know that a[m+1]>a[m]
             // that's why we're ignoring a[m]
             }
-        return ans;
+        return s;//In the end ,s will be equal to end and
+        // will be pointing to the largest number
+        //Because start and end always strive to find the largest element...therefore, when they
+        //point to a single element , it means that element is the largest.
     }
 }
