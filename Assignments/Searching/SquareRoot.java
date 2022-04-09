641f6eaded5c130a1d8a4d24f1a89828ed1bc168
nishant <nishantshah1729@gmail.com>
2022-04-09 11:11:22 +0530

    done

---
+++ Assignments/Searching/SquareRoot.java
@@ -0,0 +1,13 @@
+package com.company.Assignments.Searching;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        System.out.println(square(16));
+    }
+
+    private static double square(int n) {
+        int sr=-1;
+
+        return sr;
+    }
+}
