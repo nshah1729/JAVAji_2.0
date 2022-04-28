e513bc16fd928a884d3f494ddc290d7af2f990d8
nishant <nishantshah1729@gmail.com>
2022-04-28 10:29:42 +0530

    done

--- Assignments/Recursion/TryingRecursion.java
+++ Assignments/Recursion/TryingRecursion.java
@@ -6,10 +6,16 @@ public class TryingRecursion {
 //        System.out.println(fac(10));
 //        zigzag(2);
 //        toh(3,1,2,3);
-        int[] a={1,2,3,4};
+        int[] a={1,4,2,3,4,3,4,1};
 //        printReverse(a, a.length-1);
 //        System.out.println(max(a,0));
 //        System.out.println(min(a,0));
+//        System.out.println(firstIndex(a,0,4));
+//        System.out.println(firstIndexAliter(a,0,4));
+//        System.out.println(LastIndex(a,a.length-1,4));
+//        System.out.println(LastIndexAliter(a,a.length-1,4));
+//        AllIndices(a,0,4);
+//        System.out.println(AllIndices(a,a.length-1,4));
     }
 
     private static int max(int[] a,int idx){
@@ -75,4 +81,42 @@ public class TryingRecursion {
         System.out.print(a[i]+" ");
         printReverse(a,i-1);
     }
+
+    private static int firstIndex(int[] a,int idx,int target){
+        if(idx==a.length-1)return idx;
+        int first=-1;
+        if(a[idx]==target)first=idx;
+        else first=firstIndex(a,idx+1,target);
+        return first;
+    }
+
+    private static int firstIndexAliter(int[] a,int idx,int target){
+        if(idx==a.length)return -1;
+        if(a[idx]==target)return idx;
+        else return firstIndexAliter(a,idx+1,target);
+
+    }
+
+    private static int LastIndex(int[] a,int idx,int target){
+        if(idx==0)return a[idx];
+            int last=-1;
+            if(a[idx]==target)last=idx;
+            else last=LastIndex(a,idx-1,target);
+            return last;
+    }
+
+    private static int LastIndexAliter(int[] a,int idx,int target){
+        if(idx==-1)return -1;
+            int last=-1;
+            if(a[idx]==target)return idx;
+            else return LastIndexAliter(a,idx-1,target);
+    }
+
+    private static void AllIndices(int[] a,int idx,int target){
+        if(idx==a.length)return;
+
+       if(a[idx]==target) System.out.println(idx);
+        AllIndices(a,idx+1,target);
+    }
+
 }
